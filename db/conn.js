ec698528407adef314d5f56ea99e004537532096
hassannaveed24 <48442430+hassannaveed24@users.noreply.github.com>
2020-08-03 00:46:03 +0500

    Removing db conn string

--- db/conn.js
+++ db/conn.js
@@ -1,21 +1,8 @@
-// const mongoose = require("mongoose");
-
 const mongoose = require("mongoose");
-
-// const db = process.env.db;
-
-// mongoose
-//   .connect(db, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   })
-//   .then(() => console.log("Connected to MongoDB.."))
-//   .catch((err) => console.log(err));
 
 const db =
   process.env.db ||
-  "mongodb+srv://posuser:1234@posdb-0yxjw.mongodb.net/clothing-store?retryWrites=true&w=majority";
+  "";
 
 mongoose
   .connect(db, {
